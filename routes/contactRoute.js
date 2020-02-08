c9a0e062133b2282fe633efb8e73f5b2d77a7c13
Lucas Aho <aholucas11@gmail.com>
2020-02-08 02:41:52 -0700

    google api thingy hopefully

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,6 +1,17 @@
 //Nodemailer js import
 const nodemailer = require('nodemailer');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
+const oauth2Client = new OAuth2(
+    "1044440592107-1sc0l94kpgpe3gvpcsoqgoqt2n38mtkg.apps.googleusercontent.com", // ClientID
+    "_93EoY3sF53eVzkoy-zpj7-K", // Client Secret
+    "https://developers.google.com/oauthplayground" // Redirect URL
+);
+oauth2Client.setCredentials({
+    refresh_token: "1//040rPksjSnAp6CgYIARAAGAQSNwF-L9IrxK1Zc0APK9LGLGegP5ipib66UKPW9rWXmMmpnG6Y15mAFzdD1k3Ug8Eyzu7AZBXcijE"
+});
+const accessToken = oauth2Client.getAccessToken()
 //Env variables to store messages
 const GMAIL_USER = process.env.GMAIL_USER
 const GMAIL_PASS = process.env.GMAIL_PASS
